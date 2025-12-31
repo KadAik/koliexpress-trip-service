5ce781588dfa5eceeae373f553e39d39c8391bf3
Kadolphe <kad.build.dev@gmail.com>
2025-12-31 14:33:58 +0100

    feat: add global validation error handler

--- trip-service/src/main/java/com/koliexpress/tripservice/exceptions/handlers/GlobalExceptionHandler.java
+++ trip-service/src/main/java/com/koliexpress/tripservice/exceptions/handlers/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.koliexpress.tripservice.exceptions.handlers;
 
+import com.koliexpress.tripservice.dto.error.ValidationErrorResponseDto;
 import com.koliexpress.tripservice.exceptions.InvalidArgumentException;
 import com.koliexpress.tripservice.exceptions.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
@@ -18,9 +23,38 @@ public class GlobalExceptionHandler {
                 .body(ex.getMessage());
     }
     @ExceptionHandler(InvalidArgumentException.class)
-    public ResponseEntity<Map<String, String>> handleInvalidArgumentException(InvalidArgumentException ex) {
+    public ResponseEntity<Map<String, List<String>>> handleInvalidArgumentException(InvalidArgumentException ex) {
+        ValidationErrorResponseDto err = new ValidationErrorResponseDto(ex.getField());
+        err.addMessage(ex.getMessage());
         return ResponseEntity
                 .status(400)
-                .body(ex.getErrorMap());
+                .body(
+                        Map.of(
+                                err.getField(), err.getMessages()
+                        )
+                );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationErrors(
+            MethodArgumentNotValidException ex
+    ) {
+        Map<String, String> fieldErrors = new HashMap<>();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        fieldErrors.put(
+                                error.getField(),
+                                error.getDefaultMessage()
+                        )
+                );
+
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put("status", 400);
+        response.put("error", "Validation failed");
+        response.put("errors", fieldErrors);
+
+        return ResponseEntity.badRequest().body(response);
     }
 }
